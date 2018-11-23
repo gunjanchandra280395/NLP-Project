2c5c12dcdfdb552cd59dabf333390fd72161507d
gunjanchandra280395 <gunjanchandra280395@gmail.com>
2018-11-23 14:15:22 +0200

    Update jaccard.py

--- jaccard.py
+++ jaccard.py
@@ -1,9 +1,12 @@
-import nltk as nltk;
+import tensorflow as tf;
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+c = tf.placeholder(tf.float32)
 a={1,2,3,4,5,6,7,8,9};
 b={1,4,7};
 #a={1 2 3 4 5 6 7 8 9};
 #b={1 5 7};
 c=intersection(a,b);
-sess=nltk.Session()
+sess=tf.Session()
 result=sess.run(c)
 print("Add:%s"%(result))
